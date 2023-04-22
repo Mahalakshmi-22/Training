ca23f55204d5a25b90680e6766ebf285b55227cf
Mahalakshmi-22 <mahamurugan2001@gmail.com>
2023-04-22 23:25:00 +0530

    I have complete javascripting exercise upto 16

---
+++ javascripting/array-filtering.js
@@ -0,0 +1,4 @@
+numbers=[1,2,3,4,5,6,7,8,9,10]
+//filtered=numbers.filter()
+b=numbers.filter(e=>e%2===0)
+console.log(b)
---
+++ javascripting/looping-through-array.js
@@ -0,0 +1,7 @@
+let pets=['cat','dog','rat']
+n=pets.length
+for(i=0;i<n;i++)
+{
+    pets[i]=pets[i]+'s'
+}
+console.log(pets)
