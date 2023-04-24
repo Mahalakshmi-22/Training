7b10c941f494054b80f0f32073d5ddc20237db91
Mahalakshmi-22 <mahamurugan2001@gmail.com>
2023-04-24 13:10:42 +0530

    map exercise

---
+++ map/capitalized.js
@@ -0,0 +1,11 @@
+let a=["maha",'uma','ragul','deva']
+let b=a.map(e=>e.charAt(0).toUpperCase()+e.slice(1))
+
+//    return e
+// let c=e.slice(1)
+// // let cap=b+c
+// })
+// // let c=a.slice(1)
+// let cap=b+c
+
+console.log(b)
